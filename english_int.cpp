4514ffa9d13b6fe3da90c8b130d08e4d0bcfac11
lcramer3 <lcramer3@mail.csuchico.edu>
2018-06-14 22:39:41 -0700

    pretty fuck close

--- english_int.cpp
+++ english_int.cpp
@@ -57,8 +57,6 @@ string int_to_english(int input) {
       cout << "" << "FUCK"  << ' ';
     }
 
-
-
     //output += get_number_string(temp_input);
     j /= 10;
     k *= 10;
@@ -66,104 +64,70 @@ string int_to_english(int input) {
   return output;
 }
 
-
 string get_number_string(int input) {
-
-
   switch(input) {
     case 0:
       return "zero";
-
     case 1:
       return "one";
-
     case 2:
       return "two";
-
     case 3:
       return "three";
-
     case 4:
       return "four";
-
     case 5:
       return "five";
-
     case 6:
       return "six";
-
     case 7:
       return "seven";
-
     case 8:
       return "eight";
-
     case 9:
       return "nine";
-
     case 10:
       return "ten";
-
     case 11:
       return "eleven";
-
     case 12:
       return "twelve";
-
     case 13:
       return "thirteen";
-
     case 14:
       return "fourteen";
-
     case 15:
       return "fifteen";
-
     case 16:
       return "sixteen";
-
     case 17:
       return "seventeen";
-
     case 18:
       return "eightteen";
-
     case 19:
       return "nineteen";
-
     case 20:
       return "twenty";
-
     case 30:
       return "thirty";
-
     case 40:
       return "fourty";
-
     case 50:
       return "fifty";
-
     case 60:
       return "sixty";
-
     case 70:
       return "seventy";
-
     case 80:
       return "eighty";
-
     case 90:
       return "ninety";
-
     case 100:
       return "hundred";
-
     case 1000:
       return "thousand";
-
     case 1000000:
       return "million";
-
     case 1000000000:
       return "billion";
     default:
