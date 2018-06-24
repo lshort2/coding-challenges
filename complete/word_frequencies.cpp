f4c3e0a06a32d545b23aafef473e511df55dffd2
Lukey <lshort2@mail.csuchico.edu>
2018-06-23 17:51:34 -0700

    added gitignore

---
+++ complete/word_frequencies.cpp
@@ -0,0 +1,17 @@
+//Luke Short
+// CtCI 16.2
+#include<iostream>
+#include<string>
+#include<unordered_map>
+
+
+int main() {
+  std::unordered_map<std::string, int> hashy;
+  vector<std::string> book;
+  for(auto it:book) {
+    hashy[it]++;
+  }
+
+  return 0;
+}
+
